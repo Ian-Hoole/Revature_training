5dcac1531dc7b9a89b848f15d933f8d87362d9f2
Ian-Hoole <hoolei@sonoma.edu>
2024-09-10 08:37:14 -0700

    Completed palindromeChecker and added base file structure

---
+++ week1/palindromeChecker.js
@@ -0,0 +1,37 @@
+function isPalindrome(string){
+    let lowerIdx = 0;
+    let upperIdx = string.length-1;
+    const lowerCaseString = string.toLowerCase();
+    while(lowerIdx < upperIdx){
+        const lowerChar = lowerCaseString.charAt(lowerIdx);
+        const upperChar = lowerCaseString.charAt(upperIdx);
+        if(!isAlphaNum(lowerChar)){
+            lowerIdx++;
+        }
+        else if(!isAlphaNum(upperChar)){
+            upperIdx--;
+        }
+        else{
+            if(lowerChar !== upperChar){
+                return false;   
+            }
+            lowerIdx++;
+            upperIdx--;
+        }
+    }
+    return true;
+
+}
+
+function isAlphaNum(char){
+    const isAlpha = char.codePointAt(0) > 96 && char.codePointAt(0) < 122;
+    const isNumeric = char.codePointAt(0) > 47 && char.codePointAt(0) < 58;
+    return isAlpha || isNumeric;
+}
+
+console.log("racecar:" + isPalindrome("racecar"));
+console.log("fish:" + isPalindrome("fish"));
+console.log("abcdefghijklmnop:" + isPalindrome("abcdefghijklmnop"));
+console.log("1234567890987654321:" + isPalindrome("1234567890987654321"));
+console.log("  alice   e cil  a:" + isPalindrome("  alice   e cil  a"));
+console.log("ali  2 2 i l -==/ a:" + isPalindrome("ali  2 2 i l -==/ a"));
